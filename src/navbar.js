6c3445de3bdcd26e8583e660583d42c76855d662
Tehzeeb2487 <tehzeebjahan820@gmail.com>
2025-08-23 23:25:29 +0530

    adding some css

--- src/navbar.js
+++ src/navbar.js
@@ -15,71 +15,68 @@ const Navbar1 = () => {
   return (
     <>
       <nav className="navbar navbar1 mt-2 sticky-top">
-        <ul className="navbar-list">
-          <li className="logo-item">
-            <img src={logo} className="logo" alt="logo" />
+        <div className="navbar-left">
+          <img src={logo} className="logo" alt="logo" />
+          <div className="location-info">
+            <p className="pera">
+              Pick up<i className="fa-solid fa-angle-down"></i>
+            </p>
+            <p>Enable Location to get nearest stores</p>
+          </div>
+        </div>
+
+        {/* Hamburger Button with cross toggle */}
+        <button
+          className={`hamburger ${menuOpen ? "open" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <i className={`fa-solid ${menuOpen ? "fa-xmark" : "fa-bars"}`}></i>
+        </button>
+
+        {/* Nav Links */}
+        <ul className={`megha ${menuOpen ? "open" : ""}`}>
+          <li>
+            <a href="/" className="nav-link">
+              <i className="fa-solid fa-magnifying-glass"></i> Search
+            </a>
           </li>
-
-          <div>
-            <li className="location-info">
-              <p className="pera">
-                Pick up<i className="fa-solid fa-angle-down"></i>
-              </p>
-              <p>Enable Location to get nearest stores</p>
-            </li>
-          </div>
-
-          {/* Hamburger Button */}
-          <button
-            className="hamburger"
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
-            <i className="fa-solid fa-bars"></i>
-          </button>
-
-          {/* Links toggle */}
-          <div className={`megha ${menuOpen ? "open" : ""}`}>
-            <li>
-              <a href="/" className="nav-link">
-                <i className="fa-solid fa-magnifying-glass"></i> Search
-              </a>
-            </li>
-            <li>
-              <a href="/about" className="nav-link">
-                <i className="fa-solid fa-dice-four"></i> Categories
-              </a>
-            </li>
-            <li>
-              <a href="/services" className="nav-link">
-                <i className="fa-solid fa-percent"></i> Deals
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="nav-link">
-                <i className="fa-solid fa-cart-shopping"></i> Carts
-              </a>
-            </li>
-            <li>
-              <a href="/login" className="nav-link">
-                <i className="fa-solid fa-user"></i> Login
-              </a>
-            </li>
-          </div>
+          <li>
+            <a href="/about" className="nav-link">
+              <i className="fa-solid fa-dice-four"></i> Categories
+            </a>
+          </li>
+          <li>
+            <a href="/services" className="nav-link">
+              <i className="fa-solid fa-percent"></i> Deals
+            </a>
+          </li>
+          <li>
+            <a href="/contact" className="nav-link">
+              <i className="fa-solid fa-cart-shopping"></i> Carts
+            </a>
+          </li>
+          <li>
+            <a href="/login" className="nav-link">
+              <i className="fa-solid fa-user"></i> Login
+            </a>
+          </li>
         </ul>
       </nav>
 
-      <nav className="navbar navbar2 shadow sticky-top">
+      {/* Navbar 2 */}
+      <nav className="navbar navbar2">
         <div className="mx-auto">
           <ul>
-            <li><a href="/"><img src={g} alt="my profile"/> Grocery</a></li>
-            <li><a href="/profile"><img src={f} alt="my profile"/> Fruits & Vegetable</a></li>
-            <li><a href="/logout"><img src={m} alt="my profile"/> Fish & Meat</a></li>
-            <li><a href="/"><img src={p} alt="my profile"/> Packaged Food</a></li>
-            <li><a href="/profile"><img src={d} alt="my profile"/> Dairy & Beverages</a></li>
-            <li><a href="/settings"><img src={h} alt="my profile"/> Home & Kitchen</a></li>
+            <li><a href="/"><img src={g} alt=""/> Grocery</a></li>
+            <li><a href="/profile"><img src={f} alt=""/> Fruits & Vegetable</a></li>
+            <li><a href="/logout"><img src={m} alt=""/> Fish & Meat</a></li>
+            <li><a href="/"><img src={p} alt=""/> Packaged Food</a></li>
+            <li><a href="/profile"><img src={d} alt=""/> Dairy & Beverages</a></li>
+            <li><a href="/settings"><img src={h} alt=""/> Home & Kitchen</a></li>
           </ul>
         </div>
       </nav>
+
       <Outlet />
     </>
   );
